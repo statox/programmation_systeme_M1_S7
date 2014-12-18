8ad35fdbfacbf5f4194b2a8de90e75057e9e5669
statox <adrien.fabre92@gmail.com>
2014-12-18 04:01:28 +0100

    declaration of the allocation function of best fit

---
+++ project/src/headers/best-fit.c
@@ -0,0 +1,14 @@
+/*
+ *  Those functions implements the best-fit algorithm
+ */
+
+#ifndef BEST-FIT_H
+#define BEST-FIT_H
+
+#include "structures.h"
+#include "memory.h"
+#include <limits.h>
+
+int BFallocate (Memory memory, int requestedSize);
+
+#endif /* BEST-FIT_H */
