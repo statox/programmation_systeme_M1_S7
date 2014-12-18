17195bd0efa933e522b8ec846967342c2e386132
statox <adrien.fabre92@gmail.com>
2014-12-18 10:18:55 +0100

    creation of the file

---
+++ project/src/headers/worst-fit.h
@@ -0,0 +1,13 @@
+/*
+ *  Those functions implements the best-fit algorithm
+ */
+
+#ifndef WORSTFIT_H 
+#define WORSTFIT_H 
+
+#include "structures.h"
+#include "memory.h"
+
+int WFallocate (Memory memory, int requestedSize);
+
+#endif /* WORSTFIT_H */
