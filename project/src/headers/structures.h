bd71a99396cdb53e59e6a9b79702512c47b610ff
statox <adrien.fabre92@gmail.com>
2014-12-18 01:11:46 +0100

    base structure defining a block and the memory

---
+++ project/src/headers/structures.h
@@ -0,0 +1,30 @@
+ /*
+  * definition of the structures used by our application
+  */
+#include <stdbool.h>    // C99 definition for booleans
+
+/*
+ * Structure representing a memory block with
+ *  - its address
+ *  - its length
+ *  - the following block
+ */
+typedef struct Block Block;
+struct Block
+{
+    int address;
+    int length;
+    bool allocated;
+    struct Block *next;
+};
+
+
+/*
+ * Structure representing the whole memory composed of several blocks.
+ * It is a pointer on the first Block of the memory
+ */
+typedef Block* Memory;
+
+
+
+
